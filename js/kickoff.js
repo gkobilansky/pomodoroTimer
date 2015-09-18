97d5b7e2510a2817a2e0d6bd14f735f552e25d50
gkobilansky <genek149@gmail.com>
2015-09-18 11:23:30 -0400

    got it working

--- js/kickoff.js
+++ js/kickoff.js
@@ -1,24 +1,36 @@
-
-    var display = document.querySelector('#flowTimer');
-    var timer, length;
+$(function() {
+var display = $('#flowTimer');
+var length; 
+// var shortCount = 0;
 
     
-    document.querySelector('#go').addEventListener('click', function(e) {
+$('#go').on('click', function(e) {
         e.preventDefault();
-        length = $("input:checked").next().children().val();
-        timer = new countDownTimer(length*60, 1000);
-        timer.start();
-        timer.onTick(format);
+  /*  var type = input.prop(checked).attr('id');  // part 1 - counting short breaks             
+        if (type === 'short') {
+        shortCount += 1;
+    }  */ 
+    length = new Date().getTime() + ($("input:checked").next().children().val() * 60 * 1000);
+    
+    display.countdown(length, function(event) {
+    $(this).html(event.strftime('%M:%S'));    
+    })
+    .on('finish.countdown', function() {    
+        $(this).text('BZZZ');   
+/*         if($('input:checked').attr('id') === 'work' && shortCount%3 === 0){  //part 2 - if I've taken 3 short breaks
+          $('input').prop('checked', false);                                    // give me a long break
+          $('#long').prop('checked', true);
+        } else if ($("input:checked").attr('id') === 'short') {
+          $("input").prop('checked', false);            
+          $('#work').prop('checked', true);
+        } else {
+          $("input").prop('checked', false); 
+          $('#short').prop('checked', true);
+} */
+    
     });
+ 
+  });    
     
-     function restart() {
-        if (this.expired()) {
-            setTimeout(function() { timer.start();}, 1000);
-        }
-    } 
-    
-    function format(minutes, seconds) {
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-        display.textContent = minutes + ':' + seconds;
-    }
+});
+
